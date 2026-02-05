fc0bbbafdd15fe84bdd23ef50538476d4154174c
Vladikouu <curelariuvlad@gmail.com>
2026-02-05 21:21:48 +0200

    Vedem daca o iesit ceva plm

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+mongoose.connect('mongodb://localhost:27017/mydb')
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
 app.get('/', (req, res)=>{
     console.log('Testul suprem a fost atins cu succes!');
     res.send('Hello World ce zici ce faci ce sugi!');
